90cc3af922dbc963da01f490f1a335416b0d49d8
Bobzilla <bobzilla@wigle.net>
2010-04-27 12:15:54 +0000

    draw trail

--- src/net/wigle/wigleandroid/OpenStreetMapViewWrapper.java
+++ src/net/wigle/wigleandroid/OpenStreetMapViewWrapper.java
@@ -1,10 +1,14 @@
 package net.wigle.wigleandroid;
 
+import java.util.LinkedList;
+
 import org.andnav.osm.util.GeoPoint;
 import org.andnav.osm.views.OpenStreetMapView;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.util.AttributeSet;
 
@@ -12,20 +16,40 @@ import android.util.AttributeSet;
  * wrap the open street map view, to allow setting overlays
  */
 public class OpenStreetMapViewWrapper extends OpenStreetMapView {
+  private LinkedList<GeoPoint> trail = new LinkedList<GeoPoint>();
+  private Paint trailPaint = new Paint();
+  
   /**
    * XML Constructor (uses default Renderer)
    */
   public OpenStreetMapViewWrapper(Context context, AttributeSet attrs) {
     super( context, attrs );
+    int color = Color.argb(200, 200, 128, 200);
+    trailPaint.setColor( color );
   }
   
   @Override
   public void onDraw( Canvas c ) {
     super.onDraw( c );
     
-    // draw center crosshairs
     final GeoPoint center = this.getMapCenter();
-    final Point centerPoint = this.getProjection().toMapPixels(center, null);
+    
+    if ( trail.isEmpty() || ! trail.getLast().equals( center ) ) {
+      trail.add( center );
+    }
+    
+    // keep it from getting out of hand
+    while ( trail.size() > 1024 ) {
+      trail.removeFirst();
+    }
+    
+    for ( GeoPoint geoPoint : trail ) {
+      final Point point = this.getProjection().toMapPixels( geoPoint, null );
+      c.drawCircle(point.x, point.y, 2, trailPaint);
+    }
+    
+    // draw center crosshairs
+    final Point centerPoint = this.getProjection().toMapPixels( center, null );
     c.drawLine( centerPoint.x, centerPoint.y - 9, centerPoint.x, centerPoint.y + 9, mPaint );
     c.drawLine( centerPoint.x - 9, centerPoint.y, centerPoint.x + 9, centerPoint.y, mPaint );
   }

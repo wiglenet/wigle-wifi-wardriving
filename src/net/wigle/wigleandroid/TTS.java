2b5078f327adc8bea03fdd5b7abf4788fce1bc83
Uhtu <uhtu@wigle.net>
2010-05-02 04:24:23 +0000

    add support for TTS in android 1.5 via TTS Extended if present, TTS_library_stub.jar is the supporting libs for that package

--- src/net/wigle/wigleandroid/TTS.java
+++ src/net/wigle/wigleandroid/TTS.java
@@ -1,3 +1,6 @@
+// -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vim:ts=2:sw=2:tw=80:et
+
 package net.wigle.wigleandroid;
 
 import java.lang.reflect.Constructor;
@@ -13,7 +16,36 @@ import android.content.Context;
 public class TTS {
   private static Class SPEECH_CLASS;
   private static Class LISTENER_CLASS;
-  
+
+  // http://groups.google.com/group/tts-for-android/browse_thread/thread/a6db26ac63a5bbb3 "Using TTS Extended on Android 1.5"
+  // we link against a copy of http://eyes-free.googlecode.com/svn/trunk/commonlibs/TTS_library_stub.jar
+  /* 
+     quote:
+
+     I had great trouble, but finally got it working.
+
+     Key lessons I learned:
+
+     1.  Use Library stub 1.7 -- nothing later.  Later ones are
+     incompatible with Android 1.5
+     2.  Any APK for the extender versioned 1.7 to at least 2.0 seems to
+     work
+     3.  When using TTS on Android 1.5, you have to use the old TTS class,
+     not TextToSpeechBeta
+     4.  To get the end-of-speech callbacks to work, you have to set the
+     listener FIRST thing in your init listener -- no later.
+     
+     These lessons were hard to discover and learn, and the documentation
+     could easily be improved to minimize the ramp-up time. 
+   */
+
+  /** this had been a lovely interface/innerclass dance; but we do love us some hax 
+      (if there is ever a third engine/case be sure to go and do this right, you bastard). 
+      set true on the bad (pre-donut) path */
+  private static boolean useEyesFree = false;
+  /** hint on the 1.5 tts voice to use */
+  private static String[] params = new String[]{"VOICE_FEMALE"};
+
   @SuppressWarnings("unused")
   private static int QUEUE_FLUSH;
   private static int QUEUE_ADD;
@@ -26,30 +58,54 @@ public class TTS {
   static {
     try {
       SPEECH_CLASS = Class.forName("android.speech.tts.TextToSpeech");
-      for ( Class clazz : SPEECH_CLASS.getClasses() ) {
-        WigleAndroid.info("class: " + clazz.getCanonicalName() );
-        if ( "android.speech.tts.TextToSpeech.OnInitListener".equals( clazz.getCanonicalName() ) ) {
-          LISTENER_CLASS = clazz;
-          break;
-        }
-      }      
-      
-      QUEUE_FLUSH = SPEECH_CLASS.getDeclaredField("QUEUE_FLUSH").getInt(null);
-      QUEUE_ADD = SPEECH_CLASS.getDeclaredField("QUEUE_ADD").getInt(null);
-      speak = SPEECH_CLASS.getMethod( "speak", String.class, int.class, HashMap.class );
-      shutdown = SPEECH_CLASS.getMethod( "shutdown", new Class[]{} );
     }
     catch ( ClassNotFoundException ex ) {
-      // don't have it
+        // don't have it
     }
-    catch ( NoSuchFieldException ex ) {
-      WigleAndroid.error( "no such field: " + ex );
+    // try the eyes-free TTS.
+    if ( SPEECH_CLASS == null ) {
+        try {
+          SPEECH_CLASS = Class.forName("com.google.tts.TTS"); // naboo foxes, TTS out for the jedi!
+            useEyesFree = true;
+        } catch ( ClassNotFoundException ex ) {
+            // don't have it
+        }
     }
-    catch ( IllegalAccessException ex ) {
-      WigleAndroid.error( "illegal in static: " + ex );
-    }
-    catch ( NoSuchMethodException ex ) {
-      WigleAndroid.error( "no such method: " + ex );
+
+    if ( SPEECH_CLASS != null ) {
+      try {
+        for ( Class clazz : SPEECH_CLASS.getClasses() ) {
+          String cname =  clazz.getCanonicalName();
+          WigleAndroid.info("class: " +cname );
+          if ( "android.speech.tts.TextToSpeech.OnInitListener".equals( cname )
+               ||
+               ( useEyesFree && "com.google.tts.TTS.InitListener".equals( cname ) ) 
+               ) {
+            LISTENER_CLASS = clazz;
+            break;
+          }
+        }
+      
+        if ( useEyesFree ) {
+          speak = SPEECH_CLASS.getMethod( "speak", String.class, int.class, String[].class );
+          shutdown = SPEECH_CLASS.getMethod( "shutdown", new Class[]{} );
+          QUEUE_ADD = 1;
+        } else {
+          QUEUE_FLUSH = SPEECH_CLASS.getDeclaredField("QUEUE_FLUSH").getInt(null);
+          QUEUE_ADD = SPEECH_CLASS.getDeclaredField("QUEUE_ADD").getInt(null);
+          speak = SPEECH_CLASS.getMethod( "speak", String.class, int.class, HashMap.class );
+          shutdown = SPEECH_CLASS.getMethod( "shutdown", new Class[]{} );
+        }
+      }
+      catch ( NoSuchFieldException ex ) {
+        WigleAndroid.error( "no such field: " + ex );
+      }
+      catch ( IllegalAccessException ex ) {
+        WigleAndroid.error( "illegal in static: " + ex );
+      }
+      catch ( NoSuchMethodException ex ) {
+        WigleAndroid.error( "no such method: " + ex );
+      }
     }
     
   }
@@ -60,7 +116,12 @@ public class TTS {
   
   public TTS( Context context ) {
     try {
-      Constructor construct = SPEECH_CLASS.getConstructor( Context.class, LISTENER_CLASS );
+      Constructor construct;
+      if (useEyesFree) {
+        construct = SPEECH_CLASS.getConstructor( Context.class, LISTENER_CLASS, boolean.class );
+      } else {
+        construct = SPEECH_CLASS.getConstructor( Context.class, LISTENER_CLASS );
+      }
       InvocationHandler handler = new InvocationHandler() {
         public Object invoke( Object object, Method method, Object[] args ) {
           WigleAndroid.info("invoke: " + method.getName() );
@@ -70,7 +131,11 @@ public class TTS {
       listener = Proxy.newProxyInstance(LISTENER_CLASS.getClassLoader(), 
           new Class[]{ LISTENER_CLASS }, handler);
       
-      speech = construct.newInstance( context, listener );
+      if ( useEyesFree ) {
+        speech = construct.newInstance( context, listener, true );
+      } else {
+        speech = construct.newInstance( context, listener );
+      }
       //Method setLocation = SPEECH_CLASS.getMethod( "setLanguage", Locale.class );
       //setLocation.invoke( speech, Locale.UK );
     }
@@ -91,7 +156,11 @@ public class TTS {
   public void speak( String string ) {
     try {
       // WigleAndroid.info("saying: " + string );
-      speak.invoke( speech, string, QUEUE_ADD, (HashMap<String,String>) null );
+      if ( useEyesFree ) {
+        speak.invoke( speech, string, QUEUE_ADD, params );
+      } else {
+        speak.invoke( speech, string, QUEUE_ADD, (HashMap<String,String>) null );
+      }
     }
     catch ( IllegalAccessException ex ) {
       WigleAndroid.error( "illegal: " + ex );
@@ -104,7 +173,7 @@ public class TTS {
   public void shutdown() {
     try {
       // WigleAndroid.info("saying: " + string );
-      shutdown.invoke( speech, new Object[]{} );
+      shutdown.invoke( speech, (Object[])null );
     }
     catch ( IllegalAccessException ex ) {
       WigleAndroid.error( "illegal: " + ex );

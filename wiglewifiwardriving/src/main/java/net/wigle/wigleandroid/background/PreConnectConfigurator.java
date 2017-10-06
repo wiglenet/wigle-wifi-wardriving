1517c75350b3f17e7d2032001460dafb2f789885
rksh <arkasha@wigle.net>
2017-10-05 18:36:37 -0700

    re-working the observation uploader to try and handle connection errors more gracefully.

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/background/PreConnectConfigurator.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/background/PreConnectConfigurator.java
@@ -1,10 +1,11 @@
 package net.wigle.wigleandroid.background;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 
 /**
  * Created by bobzilla on 11/14/15
  */
 public interface PreConnectConfigurator {
-    void configure(final HttpURLConnection connection);
+    void configure(final HttpURLConnection connection) throws IOException;
 }

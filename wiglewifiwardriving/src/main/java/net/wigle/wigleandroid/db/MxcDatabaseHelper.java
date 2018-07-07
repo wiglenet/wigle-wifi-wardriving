e2fae74e47485044227b8749c4d9b7c82a2b3c6f
rksh <arkasha@wigle.net>
2018-07-06 23:21:23 -0700

    mxc record

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/db/MxcDatabaseHelper.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/db/MxcDatabaseHelper.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Environment;
 
 import net.wigle.wigleandroid.MainActivity;
+import net.wigle.wigleandroid.model.MccMncRecord;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -79,12 +80,52 @@ public class MxcDatabaseHelper extends SQLiteOpenHelper {
         //ALIBI: pre-created during build
     }
 
+    public MccMncRecord networkRecordForMccMnc(final String mcc, final String mnc) {
+        Cursor cursor = null;
+
+        if (!isPresent()) {
+            //DEBUG: MainActivity.error("No Mxc DB");
+            return null;
+        }
+
+        try {
+            if (openDataBase()) {
+                cursor = db.rawQuery(OPERATOR_FOR_MCC_MNC, new String[]{mcc, mnc});
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+
+                    MccMncRecord operator = new MccMncRecord(
+                            cursor.getString(cursor.getColumnIndex("type")),
+                            cursor.getString(cursor.getColumnIndex("countryName")),
+                            cursor.getString(cursor.getColumnIndex("countryCode")),
+                            cursor.getString(cursor.getColumnIndex("mcc")),
+                            cursor.getString(cursor.getColumnIndex("mnc")),
+                            cursor.getString(cursor.getColumnIndex("brand")),
+                            cursor.getString(cursor.getColumnIndex("operator")),
+                            cursor.getString(cursor.getColumnIndex("status")),
+                            cursor.getString(cursor.getColumnIndex("bands")),
+                            cursor.getString(cursor.getColumnIndex("notes")));
+                    return operator;
+                }
+            } else {
+                MainActivity.error("unable to open mcc/mnc database.");
+            }
+        } catch (SQLException sqlex) {
+            MainActivity.error("Unable to open DB: ",sqlex);
+
+        }finally {
+            if (null != cursor) {
+                cursor.close();
+            }
+        }
+        return null;
+    }
+
     public String networkNameForMccMnc(final String mcc, final String mnc) {
         Cursor cursor = null;
         String operator = null;
-        //File f = new File(context.getDatabasePath(MXC_DB_NAME).getAbsolutePath());
-        File f = new File(DATABASE_PATH+MXC_DB_NAME);
-        if (!f.exists()) {
+
+        if (!isPresent()) {
             //DEBUG: MainActivity.error("No Mxc DB");
             return null;
         }

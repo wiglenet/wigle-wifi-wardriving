5feab1420a616195b0360d595ee6349760d9d4a8
rksh <arkasha@wigle.net>
2018-09-02 18:29:01 -0700

    small cleanups to okhttp impl. and docs there's more to do before we release this feature.

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/background/OkFileUploader.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/background/OkFileUploader.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Credentials;
@@ -18,6 +19,18 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * Alternative upload to HttpFileUploader - use of OkHttp directly gives us several benefits:
+ * 1. interruptability
+ * 2. configurability
+ * 3. ability to get better debugging data and handle gracefully in case of failure.
+ * Also: "deleted code is debugged code." -orn
+ *
+ * NOTE: OkHttp is the underlying implementation in HttpUrlConnection beginning in android 4.4, but this is included for compat with android versions below that
+ *
+ * @author arkasha
+ * @date 20180901
+ */
 public class OkFileUploader {
 
     /**
@@ -26,6 +39,19 @@ public class OkFileUploader {
     private OkFileUploader() {
     }
 
+
+    /**
+     * simple upload method. Analogous to {@link net.wigle.wigleandroid.background.HttpFileUploader#upload HttpFileUploader.upload(...)}
+     * @param urlString the URL string for the target
+     * @param filename the file name on-device to upload
+     * @param fileParamName parameter name for the file
+     * @param params additional parameters ("donate" at time of implementation)
+     * @param authUser auth user parameter for Basic authentication
+     * @param authToken password parameter for Basic authentication
+     * @param handler the callback for progress or completion
+     * @return string payload (JSON) from the upload POST
+     * @throws IOException
+     */
     public static String upload(final String urlString, final String filename, final String fileParamName,
                                 final Map<String, String> params,
                                 final String authUser, final String authToken,
@@ -33,11 +59,14 @@ public class OkFileUploader {
             throws IOException {
 
 
+        // construct multipart body using file reference
         MultipartBody.Builder builder = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
                 .addFormDataPart("file", fileParamName,
                         RequestBody.create(MediaType.parse("application/octet-stream"),
                                 new File(filename)));
+
+        // add params if present
         if (!params.isEmpty()) {
             for ( Map.Entry<String, String> entry : params.entrySet() ) {
 
@@ -48,6 +77,7 @@ public class OkFileUploader {
         MultipartBody requestBody = builder.build();
 
 
+        // progress-aware requestBody
         CountingRequestBody countingBody
                 = new CountingRequestBody(requestBody, new CountingRequestBody.Listener() {
             @Override
@@ -60,6 +90,7 @@ public class OkFileUploader {
             }
         });
 
+        // authorization if necessary
         Request request;
         if ((authToken != null) && !authToken.isEmpty()) {
             request = new Request.Builder()
@@ -74,7 +105,10 @@ public class OkFileUploader {
                     .build();
         }
 
-        OkHttpClient client = new OkHttpClient.Builder().build();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS).build();
 
         Call call = client.newCall(request);
         Response response = call.execute();
@@ -86,6 +120,4 @@ public class OkFileUploader {
         }
 
     }
-
 }
-

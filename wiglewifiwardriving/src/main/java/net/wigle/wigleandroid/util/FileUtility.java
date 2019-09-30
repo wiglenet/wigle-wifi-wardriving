378ce2b47ab7a23b7aaf723afd0170c866719a5b
rksh <arkasha@wigle.net>
2019-09-30 09:30:47 -0700

    freespace wrapper

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/util/FileUtility.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/util/FileUtility.java
@@ -4,6 +4,8 @@ import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
 
+import net.wigle.wigleandroid.MainActivity;
+
 import java.io.File;
 
 /**
@@ -20,11 +22,17 @@ public class FileUtility {
 
     //based on the smart answer in https://stackoverflow.com/questions/7115016/how-to-find-the-amount-of-free-storage-disk-space-left-on-android
     public static long getFreeBytes(File path) {
-        StatFs stats = new StatFs(path.getAbsolutePath());
-        if (Build.VERSION.SDK_INT >= 18) {
-            return stats.getAvailableBlocksLong() * stats.getBlockSizeLong();
-        } else {
-            return (long) (stats.getAvailableBlocks() * stats.getBlockSize());
+        try {
+            StatFs stats = new StatFs(path.getAbsolutePath());
+            if (Build.VERSION.SDK_INT >= 18) {
+                return stats.getAvailableBlocksLong() * stats.getBlockSizeLong();
+            } else {
+                return (long) (stats.getAvailableBlocks() * stats.getBlockSize());
+            }
+        } catch (Exception ex) {
+            // if we can't determine freespace, be optimistic.
+            MainActivity.error("Unable to determine free space: ",ex);
+            return Long.MAX_VALUE;
         }
     }
 

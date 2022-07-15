a647a7f45729fce496ece3395140a4cd94bd24de
Krzysztof Zoń <krzysztof.zon2001@gmail.com>
2022-07-16 01:05:27 +0200

    OUI availability check implementation

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/model/OUI.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/model/OUI.java
@@ -1,9 +1,7 @@
 package net.wigle.wigleandroid.model;
 
 import android.content.res.AssetManager;
-
 import net.wigle.wigleandroid.util.Logging;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,21 +9,38 @@ import java.util.Properties;
 
 public final class OUI {
     private final Properties properties = new Properties();
+    private final boolean propertiesAvailable = false;
+
     public OUI(final AssetManager assetManager) {
         try {
-            final InputStream stream = assetManager.open("oui.properties");
-            Logging.info("oui stream: " + stream);
+            final InputStream ouiStream = assetManager.open("oui.properties");
+            Logging.info("Oui stream: " + stream);
 
-            InputStreamReader isr = new InputStreamReader(stream, "UTF-8");
-            properties.load(isr);
-            Logging.info("oui load complete");
-        }
-        catch (final IOException ex) {
-            Logging.error("exception loading oui: " + ex, ex);
+            InputStreamReader ouiStreamReader = new InputStreamReader(ouiStream, "UTF-8");
+            properties.load(ouiStreamReader);
+            Logging.info("Oui properites loaded successful");
+
+            propertiesAvailable = true;
+        
+        } catch (final IOException ex) {
+            Logging.error("Oui properties loading failed: " + ex, ex);
+            propertiesAvailable = false;
         }
     }
 
+    /**
+    * Returns the name of the manufacturer assigned to a given OUI or null value when the OUI does not exist or the data is not available.
+    */
     public String getOui(final String partial) {
+        if (!propertiesAvailable) return null;
+        
         return properties.getProperty(partial);
+    }
+
+    /**
+    * Returns a boolean value informing whether the OUI data was successfully loaded and whether it is available.
+    */
+    public boolean isOuiAvailable() {
+        return propertiesAvailable;
     }
 }

ac99c9fe68662aa7658aa26c951d0a522fac0e31
Andy Carra <rksh@wigle.net>
2023-08-07 17:32:59 -0700

    removing unused include

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/net/WiGLEApiManager.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/net/WiGLEApiManager.java
@@ -39,7 +39,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.SocketTimeoutException;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 

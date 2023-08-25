57cb6108bec45598a99384c6ad96f3e15df416ef
Andy Carra <rksh@wigle.net>
2023-08-24 22:45:39 -0700

    could result in nulls.

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/listener/BluetoothReceiver.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/listener/BluetoothReceiver.java
@@ -684,8 +684,11 @@ public final class BluetoothReceiver extends BroadcastReceiver implements LeScan
             network.setSsid(mergedSsid);
         } else {
             //DEBUG: MainActivity.info("existing BT net: "+network.getBssid() + "(new: "+newForRun+")");
-            //TODO: update capabilities? only if was Misc/Uncategorized, now recognized?
-            network.setCapabilities(capabilities);
+            //TODO: update capabilities only if was Misc/Uncategorized, now recognized?
+            //if (capabilities != null && !capabilities.isEmpty() && (network.getCapabilities().isEmpty() || network.getCapabilities().startsWith("Misc") || network.getCapabilities().startsWith("Uncategorized") )) {
+            //    network.setCapabilities(capabilities);
+            // ALIBI: device state/bond state not available in this method to post-pend;
+            //}
             network.setLevel(strength);
             network.setFrequency(deviceType);
             if (null != ssid) {

47d104848240886acb13383b0426bf41fdbb58da
Andy Carra <andy.carra@gmail.com>
2023-09-07 15:53:16 -0700

    trying to defy system notification BG not working well (#630)
    Co-authored-by: Andy Carra <rksh@wigle.net>

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/WigleService.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/WigleService.java
@@ -378,10 +378,6 @@ public final class WigleService extends Service {
             builder.setColorized(true);
             builder.setOnlyAlertOnce(true); //ALIBI: prevent multiple badge notification
 
-            if (SDK_INT < 29) {
-                //Classic charcoal:
-                builder.setColor(1973790);
-            }
             //TODO: figure out how to update notification actions on exec, then we can show relevant
             if (MainActivity.isScanning(getApplicationContext())) {
                 Notification.Action pauseAction = new Notification.Action.Builder(

1952e02fbdc1ccc6e7f84f9320ff113675ea019b
Mark Grayson <mgrayson@cisco.com>
2023-10-27 11:48:08 +0100

    Add HS2.0 to capabilities list

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/model/Network.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/model/Network.java
@@ -120,7 +120,7 @@ public final class Network implements ClusterItem {
      * @param scanResult a result from a wifi scan
      */
     public Network( final ScanResult scanResult ) {
-        this( scanResult.BSSID, scanResult.SSID, scanResult.frequency, scanResult.capabilities,
+        this( scanResult.BSSID, scanResult.SSID, scanResult.frequency, scanResult.isPasspointNetwork() ? (scanResult.capabilities + "[HS2.0]",
                 scanResult.level,  NetworkType.WIFI, null, null, null);
 
         String rcois = null;

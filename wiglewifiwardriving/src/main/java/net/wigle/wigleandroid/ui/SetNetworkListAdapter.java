b4133ecae59ba188c0545841e374142dc1122c96
Andy Carra <andy.carra@gmail.com>
2024-01-20 17:44:08 -0800

    removing manual spacing, because what? (#674)
    Co-authored-by: Andy Carra <rksh@wigle.net>

--- wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/ui/SetNetworkListAdapter.java
+++ wiglewifiwardriving/src/main/java/net/wigle/wigleandroid/ui/SetNetworkListAdapter.java
@@ -217,7 +217,7 @@ public final class SetNetworkListAdapter extends AbstractListAdapter<Network> {
         }
 
         TextView tv = row.findViewById(R.id.ssid);
-        tv.setText(network.getSsid() + " ");
+        tv.setText(network.getSsid());
 
         tv = row.findViewById(R.id.oui);
         final String ouiString = network.getOui(ListFragment.lameStatic.oui);
